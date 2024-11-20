8ea17118649b044cb63ef055270ab6d2c49d970a
CheesecakeSeal <cheesecakeseal@gmail.com>
2024-11-20 23:32:05 +0100

    Local Host Calculator App

---
+++ src/main/java/org/cheesecakeseal/softwaretesting/SampleCalcAppCalculationResponse.java
@@ -0,0 +1,17 @@
+package org.cheesecakeseal.softwaretesting;
+
+public class SampleCalcAppCalculationResponse {
+    private double result;
+
+    public SampleCalcAppCalculationResponse(double result) {
+        this.result = result;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+}
